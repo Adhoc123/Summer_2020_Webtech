07be117ec171c07b2582dc86ae745a8fa68a97ae
Adhoc123 <emonhasan1010@gmail.com>
2020-09-01 01:19:10 +0600

    show

---
+++ Final Task 2/Degree/show.php
@@ -0,0 +1,12 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+        if(!isset($_REQUEST['degree']))echo "Invalid Degree!";
+        else {
+            $degree = $_REQUEST['degree'];
+            echo "Submit Successful!";
+        }
+  
+    }
+?>
