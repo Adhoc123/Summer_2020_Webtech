476ce2966696c03653c88b1826c5a7b7b035b8f9
Adhoc123 <emonhasan1010@gmail.com>
2020-09-01 01:25:03 +0600

    show

---
+++ Final Task 2/Blood grp/show.php
@@ -0,0 +1,9 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+        $blood =$_REQUEST['blood'];
+        if(empty($blood))echo "Invalid Blood Group!";
+        else echo "Submit Successful!";
+    }
+?>
